16261975b6da23b5601fe2f9358914e4907d499a
EvelinyStark <38438845+EvelinyStark@users.noreply.github.com>
2025-08-04 17:29:15 +0300

    emocija

--- main.py
+++ main.py
@@ -277,7 +277,7 @@ def home():
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Jāņa & Annas Vides Mākslas Sarunas | Environmental Art Exhibition</title>
-    <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🎨</text></svg>">
+    <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>✨</text></svg>">
     <style>
         * { margin: 0; padding: 0; box-sizing: border-box; }
         
